de43d2587183a648c492d766b0949ce7d7b39537
Don Naro <dnaro@redhat.com>
2023-03-03 12:02:20 +0000

    incorporate oldsite toggle

--- build.py
+++ build.py
@@ -28,4 +28,5 @@ if __name__ == "__main__":
 
 
     shutil.copytree('static', 'build/static')
+    shutil.copytree('oldsite', 'build/oldsite')
     sass.compile(dirname=('sass', 'build/static/css'))
